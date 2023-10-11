c175246f4c03d2838ff6379c42a6f0a39f8f9812
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-10-11 11:28:43 +0000

    [Feature]: Updated BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the class with public instance attributes
 
@@ -25,9 +25,20 @@ class BaseModel:
         updated_at: uses datetime to assign the current datetime when an
             instance is created & updated everytime the object is changed
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            if "created_at" in kwargs:
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """
