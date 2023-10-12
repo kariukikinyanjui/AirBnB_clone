892effbaac4ef145c2a1b26e09468cd11282112b
Tai Keith <keithsteve.ks@hotmail.com>
2023-10-12 06:15:31 -0700

    [Feature]: Fixed BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the class with public instance attributes
 
@@ -25,9 +25,18 @@ class BaseModel:
         updated_at: uses datetime to assign the current datetime when an
             instance is created & updated everytime the object is changed
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """
