e2e590ecabb980aa5b090e322d1e7d09a9b542be
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-10-13 07:57:47 +0000

    [Feature]: Updated the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,10 @@
 This module contains a class that defines all common attributes/methods
 for other classes
 """
+import models
 import uuid
 from datetime import datetime
-from models import storage
+
 
 
 class BaseModel:
@@ -34,7 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """
@@ -42,7 +43,7 @@ class BaseModel:
         'updated_at' with the current time
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
