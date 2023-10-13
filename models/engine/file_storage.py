547557305af89c0e9c9ac08a488dfbf94cfe0de4
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-10-13 07:48:30 +0000

    [Feature]: Fixed the reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ This module can serialize instance to a JSON file and deserialize
 JSON files back into instances.
 """
 import json
+from models.base_model import BaseModel
 
 """Define a class FileStorage"""
 
@@ -12,8 +13,6 @@ class FileStorage:
     """class=-level attributes to store file path and objects."""
     __file_path = "file.json"
     __objects = {}
-    # Define the 'models' dictionary to store class references.
-    models = {}
 
     def all(self):
         """Retrieves all objects."""
@@ -21,27 +20,24 @@ class FileStorage:
 
     def new(self, obj):
         """Adds a new object to the storage."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        key = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(key, obj.id)] = obj
 
     def save(self):
         """Serialize and save objects to a JSON file."""
-        data = {}
-        for key, obj in FileStorage.__objects.items():
-            data[key] = obj.to_dict()
+        data = FileStorage.__objects
+        data = {obj: data[obj].to_dict() for obj in data.keys()}
         with open(FileStorage.__file_path, "w") as file:
             json.dump(data, file)
 
     def reload(self):
         """Deserialize objects from the JSON file and populate the storage."""
         try:
-            with open(FileStorage.__file_path, "r") as file:
+            with open(FileStorage.__file_path) as file:
                 data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split(".")
-                    cls = FileStorage.models.get(class_name)
-                    if cls:
-                        instance = cls(**value)
-                        FileStorage.__objects[key] = instance
+                for d in data.values():
+                    cls_name = d["__class__"]
+                    del d["__class__"]
+                    self.new(eval(cls_name)(**d))
         except FileNotFoundError:
-            pass
+            return
